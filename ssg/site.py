17dd506a078242580165606dee5fb0805f09fbda
Hao Cheng <hao.cheng@sas.com>
2023-04-10 11:27:30 -0400

    update

--- ssg/site.py
+++ ssg/site.py
@@ -13,15 +13,6 @@ class Site:
         directory.mkdir(parents=True, exist_ok=True)
 
 
-    def build(self):
-        self.dest.mkdir(parents=True, exist_ok=True)
-        for path in self.source.rglob("*"):
-            if path.is_dir():
-                self.create_dir(path)
-            elif path.is_file():
-                self.run_parser(path)
-
-
     def load_parser(self, extension):
         for parser in self.parsers:
             if parser.valid_extension(extension):
@@ -34,3 +25,12 @@ class Site:
             parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
+
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
