fd0076d11c8512bb6c240569e1e3206b5ed9dba9
Hao Cheng <hao.cheng@sas.com>
2023-04-11 00:29:36 -0400

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -1,11 +1,11 @@
 import re
-from yaml import load, loader
+from yaml import load, FullLoader
 from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = r"^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
 
     @classmethod
